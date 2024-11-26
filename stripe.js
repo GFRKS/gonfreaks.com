b8ab495ec69dbb57e5ca92b4eb0b4dc22b0019d1
GFRKS <58492471+GFRKS@users.noreply.github.com>
2024-11-26 14:19:49 -0500

    Update stripe.js

--- stripe.js
+++ stripe.js
@@ -5,7 +5,7 @@ const stripe = Stripe('pk_live_51QPCmd008vQHUmH0tCs6YwzAgWFDutdhhL78AY6pxWUzNrIr
 document.getElementById('checkout-button').addEventListener('click', async () => {
     try {
         // Call your backend to create a checkout session
-        const response = await fetch('https://gonfreaks-8xltj9kk6-gfrks-projects.vercel.app', {
+        const response = await fetch('gonfreaks-qnx2ebpdn-gfrks-projects.vercel.app', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
         });
