b1f3e960fe8254b305c3d80cce9862bb791d5212
GFRKS <58492471+GFRKS@users.noreply.github.com>
2024-11-27 11:18:45 -0500

    Update backendcode.js

--- api/backendcode.js
+++ api/backendcode.js
@@ -29,7 +29,7 @@ module.exports = async (req, res) => {
         ],
         mode: 'payment',
         success_url: 'https://gonfreaks.com/success',
-        cancel_url: 'https://gonfreaks.com/cancel',
+        cancel_url: 'https://gonfreaks.com/',
       });
 
       res.status(200).json({ id: session.id });
